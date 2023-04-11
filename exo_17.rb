284e9c48910924e78b0c6457609774d65df4342d
Guillaume Regimbeau <gregimbeau@mail.com>
2023-04-11 15:52:55 +0200

    ok

---
+++ exo_17.rb
@@ -0,0 +1,16 @@
+puts "Renseignez un nombre entre 1 et 25"
+print ">"
+level = gets.chomp.to_i
+
+if level > 25
+  then puts "fuck off"
+elsif level < 1 
+  then puts "fuck off"
+else
+
+  for i in 0...level
+    brick_row = i*2+1
+    puts "#{" "*(level-i)}#{"#"*brick_row}"
+  end
+
+end
