45ab68df522e00bc094e029e15978c3311da4d84
jocelyn-ep <jocelyn.epaillard.pro@gmail.com>
2023-04-11 10:13:37 +0200

    ajout exo05 et modif exo03

--- exo_03.rb
+++ exo_03.rb
@@ -1,4 +1,4 @@
-puts "Renseignez votre date de naissance"
+puts "Renseignez votre année de naissance"
 print ">"
 age = gets.chomp.to_i
 puts "Vous aviez #{2017-age} ans en 2017"
---
+++ exo_05.rb
@@ -0,0 +1,6 @@
+puts "Renseignez un nombre"
+print ">"
+num = gets.chomp.to_i
+num.times do
+  puts "Salut, ça farte?"
+end
