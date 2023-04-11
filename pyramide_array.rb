34e663e23f12c30a7f26bfe4de7b783a3df14205
jocelyn-ep <jocelyn.epaillard.pro@gmail.com>
2023-04-11 15:34:14 +0200

    ajout pyramide-expert et pyramide-array en bonus

---
+++ pyramide_array.rb
@@ -0,0 +1,19 @@
+
+puts "Renseignez un nombre entre 1 et 25"
+print ">"
+level = gets.chomp.to_i
+
+if level > 25
+  then puts "fuck off"
+elsif level < 1 
+  then puts "fuck off"
+else
+
+pyramide = Array.[](
+  for i in 1..level
+    puts "#{" "*(level-i)}#{"#"*i}"
+  end 
+)
+end
+
+puts pyramide
